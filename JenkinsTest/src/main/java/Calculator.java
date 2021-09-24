2f5ea8d320aa77c2a2f679abfdd30bd3be5acab1
sudipachandra <sudipabhunia@gmail.com>
2021-09-24 13:26:03 +0530

    second commit

--- JenkinsTest/src/main/java/Calculator.java
+++ JenkinsTest/src/main/java/Calculator.java
@@ -2,4 +2,8 @@ public class Calculator {
     public int addition(int a, int b) {
         return (a+b);
     }
+
+    public int divition(int i, int j) {
+        return(i/j);
+    }
 }
--- JenkinsTest/src/test/java/SampleTest.java
+++ JenkinsTest/src/test/java/SampleTest.java
@@ -5,7 +5,13 @@ public class SampleTest {
     public void test1(){
    Calculator cal = new Calculator();
    int result = cal.addition(10,20);
-        System.out.println("result is: "+result);
+        System.out.println("result of addition is : "+result);
+    }
+    @Test
+    public void test2(){
+        Calculator cal = new Calculator();
+        int result = cal.divition(50,2);
+        System.out.println("result of division is : "+result);
     }
 
 }
